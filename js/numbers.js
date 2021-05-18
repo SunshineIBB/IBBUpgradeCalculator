9b9a180438f0d45e26ea2a8c9e539c0420a17fb6
SunshineIBB <84393604+SunshineIBB@users.noreply.github.com>
2021-05-18 09:54:12 -0400

    Commit version 1.0

---
+++ js/numbers.js
@@ -0,0 +1,49 @@
+let letters = ["", "K", "M", "B", "T", "q", "Q", "s", "S", "o", "N", "D", "aa", "ab", "ac", "ad", "ae", "af", "ag", "ah", "ai", "aj", "ak", "al", "am", "an", "ao"];
+
+function convertNumberLetter(number, letter) {
+    var finalNum;
+    var finalLet;
+
+    if (letter == undefined) {
+        letter = "";
+    }
+    
+    // current max supported value
+    if (letter == null) {
+        return number.toString()
+    }
+
+    if (letter == "ao") {
+        // make no change
+        finalNum = number;
+        finalLet = letter;
+    } else {
+        // adjust to appropriate letter
+        var index = letters.indexOf(letter);
+        if (index == -1) {
+            index = 0;
+        }
+
+        while (number > 1000) {
+            index++;
+            number /= 1000;
+        }
+        finalNum = parseFloat(number).toFixed(2);
+        finalLet = letters[index];
+    }
+    return  finalNum.toString() + finalLet;
+}
+
+function reverseNumberLetter(number, letter) {
+    
+    if (letter == undefined || letter == "" || letter == null) {
+        return number;
+    } else {
+        var index = letters.indexOf(letter);
+        while (index > 0) {
+            index--;
+            number *= 1000;
+        }
+        return number;
+    }
+}
